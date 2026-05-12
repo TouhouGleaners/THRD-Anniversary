ac435106f6b9533313e2ce36394e2e64415d54ed
Miku_oso <1612210931@qq.com>
2026-05-12 19:22:55 +0800

    ci: add github actions deployment workflow (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
   return {
+    base: '/THRD-Anniversary/',
     plugins: [vue(), tailwindcss()],
     resolve: {
       alias: {
